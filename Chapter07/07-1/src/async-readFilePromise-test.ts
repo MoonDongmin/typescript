fcde154b8defb33cba1a02837e909a4bc7c42648
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-04-07 18:50:06 +0900

    studey Chap07

---
+++ Chapter07/07-1/src/async-readFilePromise-test.ts
@@ -0,0 +1,14 @@
+import {readFilePormis} from './readFilePromise'
+
+const readFilesAll = async (filenames: string[]) => {
+    return await Promise.all(
+        filenames.map(filename => readFilePormis(filename))
+    )
+}
+
+readFilesAll(['./package.json', './tsconfig.json'])
+    .then(([packageJson, tsconfigJson]: string[]) => {
+        console.log('<package.json>:', packageJson)
+        console.log('<tsconfig.json>:', tsconfigJson)
+    })
+    .catch(err => console.log('error:', err.message))
