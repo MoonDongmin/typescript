8fb2b1e7677b0702849e89df80e0ab7746c28dd8
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-11-08 16:34:18 +0900

    Chapter 01. 타입스크립트 소개와 배경

---
+++ Chapter01/1.4.1.js
@@ -0,0 +1,15 @@
+// @ts-check
+/**
+ *@description 두 수의 합을 구하는 함수 // 함수에 대한 역할을 설명
+ *@param {number} a 첫 번째 숫자 // 함수의 파라미터와 역할과 타입을 정의
+ *@param {number} b 두 번째 숫자
+ */
+
+function sum(a, b) {
+    return a + b;
+}
+
+
+
+var total = sum(200, 300);
+total.tofi
