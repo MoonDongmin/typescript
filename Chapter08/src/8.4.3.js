2667352bc5f5a65de0e9d8fa5f525b7daf98ed18
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-12-08 16:10:23 +0900

    Chapter08. 이넘

---
+++ Chapter08/src/8.4.3.js
@@ -0,0 +1,7 @@
+var appLevel = "Error" /* logLevel1.Error */;
+var logLevel2;
+(function (logLevel2) {
+    logLevel2["Debug"] = "Debug";
+    logLevel2["Info"] = "Info";
+    logLevel2["Error"] = "Error";
+})(logLevel2 || (logLevel2 = {}));
