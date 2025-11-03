b04ee3133f19dc5010aa7fc356603ac39bcf88c8
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-11-03 09:33:33 +0900

    함수 타입 변형

---
+++ typescript-deepen/chapter01-type-transformation/00-basic/type-value-space.ts
@@ -0,0 +1,19 @@
+interface Person {
+  first: string;
+  last: string;
+}
+
+const p: Person = {
+  first: "Jane",
+  last: "Jacobs",
+};
+
+// ---
+
+const v = typeof p;
+type t = typeof p
+
+// ---
+
+const first = p["first"];
+type First = Person["first"];
