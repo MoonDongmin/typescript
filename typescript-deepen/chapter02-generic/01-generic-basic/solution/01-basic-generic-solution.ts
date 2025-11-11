2b9a753676aaf06364fdc18ae5b309e47411accb
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-11-11 11:19:47 +0900

    제네릭 기본

---
+++ typescript-deepen/chapter02-generic/01-generic-basic/solution/01-basic-generic-solution.ts
@@ -0,0 +1,13 @@
+import {
+  Equal,
+  Expect,
+} from "../../../helper";
+
+const returnWhatIPassIn = <T>(t: T) => {
+  return t;
+};
+
+const one = returnWhatIPassIn(1);
+const matt = returnWhatIPassIn("matt");
+
+type tests = [Expect<Equal<typeof one, 1>>, Expect<Equal<typeof matt, "matt">>];
---
+++ typescript-deepen/chapter02-generic/01-generic-basic/solution/04-generics-object-2-solution.ts
@@ -0,0 +1,39 @@
+import {
+  Equal,
+  Expect,
+} from "../../../helper";
+
+type CreateDataShape<T, E> = {
+  data: T;
+  error: E;
+};
+
+type tests = [
+  Expect<
+    Equal<
+      CreateDataShape<string, TypeError>,
+      {
+        data: string;
+        error: TypeError;
+      }
+    >
+  >,
+  Expect<
+    Equal<
+      CreateDataShape<number, Error>,
+      {
+        data: number;
+        error: Error;
+      }
+    >
+  >,
+  Expect<
+    Equal<
+      CreateDataShape<boolean, SyntaxError>,
+      {
+        data: boolean;
+        error: SyntaxError;
+      }
+    >
+  >,
+];
